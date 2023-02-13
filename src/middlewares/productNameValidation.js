cba835d695b20c68d90beed1b12a31e3f74c54f3
Thawane <thawaneapoliveira@gmail.com>
2023-02-13 18:22:03 -0300

    feat: adiciona requisito 4, validação para produtos cadastrados: nome não pode ser vazio ou menor que 5 caracteres

---
+++ src/middlewares/productNameValidation.js
@@ -0,0 +1,15 @@
+function productNameValidation(req, res, next) {
+  const { name } = req.body;
+
+  if (!name || name === undefined) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+
+  if (name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+
+  next();
+}
+
+module.exports = productNameValidation;
--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,9 +1,10 @@
 const productsRouter = require('express').Router();
 
 const controller = require('../controllers/products.controller');
+const productNameValidation = require('../middlewares/productNameValidation');
 
 productsRouter.get('/', controller.getAllProducts);
 productsRouter.get('/:id', controller.getProductById);
-productsRouter.post('/', controller.addNewProduct);
+productsRouter.post('/', productNameValidation, controller.addNewProduct);
 
 module.exports = productsRouter;
